35b6da1b8ccbf1a37b3c5ab71676642e58b4f246
cgpervaze <pervaze.ahanger@capgemini.com>
2025-08-29 15:37:58 -0400

    refactor(menu-item): update component styles and structure for improved layout

--- src/app/layout/menu-item/menu-item.component.ts
+++ src/app/layout/menu-item/menu-item.component.ts
@@ -8,45 +8,46 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu-item',
-  imports: [CommonModule, MatListModule, RouterModule, MatIconModule, MatExpansionModule],
+  standalone: true,
+  imports: [CommonModule, MatListModule, RouterModule, MatIconModule, MatExpansionModule, MenuItemComponent],
   template: `
     <mat-list>
       <ng-container *ngFor="let item of menu">
         <!-- If the item doesn't have children show it as list item-->
-        <ng-container *ngIf="item.subMenu === undefined">
+        <ng-container *ngIf="!item.subMenu">
           <mat-list-item
             *ngIf="item.title"
             [routerLink]="item.link"
             routerLinkActive="active"
             [routerLinkActiveOptions]="{ exact: true }"
           >
-            <mat-icon [style.color]="item.color" mat-list-icon>
+            <mat-icon [style.color]="item.color" class="mat-list-icon">
               {{ item.icon }}
             </mat-icon>
-            <div>{{ item.title }}</div>
+            <span class="list-item">{{ item.title }}</span>
           </mat-list-item>
         </ng-container>
 
         <!-- If the item has subMenu show it as accordion-->
-        <!-- <ng-container *ngIf="item.subMenu?.length > 0">
+        <ng-container *ngIf="item.subMenu?.length">
           <mat-expansion-panel [expanded]="item.expanded">
             <mat-expansion-panel-header>
-              <mat-panel-title class="d-flex align-items-center">
-                <mat-icon [style.color]="item.color" mat-list-icon>
+              <mat-panel-title>
+                <mat-icon [style.color]="item.color" class="mat-list-icon">
                   {{ item.icon }}
                 </mat-icon>
-                <span mat-line class="list-item">{{ item.title }}</span>
+                <span class="list-item">{{ item.title }}</span>
               </mat-panel-title>
             </mat-expansion-panel-header>
-            <app-menu-item [menu]="item.subMenu"></app-menu-item>
+            <app-menu-item [menu]="item.subMenu || []"></app-menu-item>
           </mat-expansion-panel>
-        </ng-container>-->
-      </ng-container> 
+        </ng-container>
+      </ng-container>
     </mat-list>
   `,
-  styleUrls: ['./menu-item.component.css'],
+  styleUrls: ['./menu-item.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuItemComponent {
-  @Input() menu: Menu = [];
+  @Input({ required: true }) menu!: Menu;
 }
