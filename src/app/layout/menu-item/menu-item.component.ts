810471aaaa5e67fdc48cfb3ebfd2f7c351e896dc
cgpervaze <pervaze.ahanger@capgemini.com>
2025-08-29 16:28:30 -0400

    refactor(menu-item): streamline template structure and improve readability

--- src/app/layout/menu-item/menu-item.component.ts
+++ src/app/layout/menu-item/menu-item.component.ts
@@ -12,28 +12,25 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule, MatListModule, RouterModule, MatIconModule, MatExpansionModule, MenuItemComponent],
   template: `
     <mat-list>
-        @for (item of menu; track item) {
-          @if (!item.subMenu?.length) {
+      @for (item of menu; track item) {
+        @if (!item.subMenu?.length) {
           <mat-list-item>
             <mat-icon matListItemIcon>{{item.icon}}</mat-icon>
             <div matListItemTitle class="list-item">{{item.title}}</div>
           </mat-list-item>
         }
-
-          @if (item.subMenu?.length) {
-             <mat-expansion-panel [expanded]="item.expanded">
+        @if (item.subMenu?.length) {
+          <mat-expansion-panel [expanded]="item.expanded">
             <mat-expansion-panel-header>
               <mat-panel-title>
-                <mat-icon [style.color]="item.color" class="mat-list-icon">
-                  {{ item.icon }}
-                </mat-icon>
-                <span class="list-item">{{ item.title }}</span>
+                <mat-icon matListItemIcon>{{item.icon}}</mat-icon>
+                <span matListItemTitle class="list-item">{{item.title}}</span>
               </mat-panel-title>
             </mat-expansion-panel-header>
             <app-menu-item [menu]="item.subMenu || []"></app-menu-item>
           </mat-expansion-panel>
-          }
         }
+      }
     </mat-list>
   `,
   styleUrls: ['./menu-item.component.sass'],
@@ -42,3 +39,4 @@ import { CommonModule } from '@angular/common';
 export class MenuItemComponent {
   @Input({ required: true }) menu!: Menu;
 }
+
